341881a740f40544d40c4edeefaa5d126a917492
Romain Lobreau <romain.lobreau@etudiant.univ-reims.fr>
2024-06-10 14:17:16 +0200

    Ajout de la classe Poster avec ses méthodes

---
+++ src/Entity/Poster.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Entity;
+
+use Database\MyPdo;
+use Entity\Exception\EntityNotFoundException;
+
+class Poster
+{
+    private int $id;
+    private string $jpeg;
+
+    public static function findById(int $id)
+    {
+        $stmt = MyPdo::getInstance()->prepare(
+            <<<'SQL'
+            SELECT id, jpeg
+            FROM poster
+            WHERE id = :ID
+        SQL
+        );
+        $stmt->execute([":ID" => $id]);
+        $poster = $stmt->fetchObject(Poster::class);
+        if ($poster === false) {
+            throw new EntityNotFoundException();
+        }
+        return $poster;
+    }
+    public  function getId(): int
+    {
+        return $this->id;
+    }
+    public  function getJpeg(): string
+    {
+        return $this->jpeg;
+    }
+
+}
